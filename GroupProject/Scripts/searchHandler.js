ca73bbd804660231173a5b458efc21537d0e2caa
Jacob Harris <jake98harris@gmail.com>
2019-04-04 15:50:22 -0500

    I FIXEDEDED THE THINGIES

--- GroupProject/Scripts/searchHandler.js
+++ GroupProject/Scripts/searchHandler.js
@@ -1,5 +1,11 @@
 ﻿$(document).ready(function () {
-
+    $('#posts2').hide();
+    $('#searchForm').on('submit',
+        function(event) {
+            event.stopImmediatePropagation();
+            event.preventDefault();
+            //Haha, no form submitting for you!
+        });
     $('#adminSearchButton').click(function (event) {
         var category = $("#searchList").val();
         var term = $("#SearchTextbox").val();
@@ -9,7 +15,7 @@
     $('#guestSearchButton').click(function (event) {
         var category = $("#searchList").val();
         var term = $("#SearchTextbox").val();
-        var urlString = 'Search/' + category + '/' + term;
+        var urlString = '/Search/' + category + '/' + term;
         HandleSearch(urlString);
     });
 });
@@ -23,14 +29,23 @@ function HandleSearch(urlString) {
             'Content-Type': 'application/json'
         },
         success: function (data, status) {
+            $('#posts2').text('');
             $.each(data,
                 function (index, item) {
-                    var blogPostId = item.BlogPostId;
-                    var title = item.BlogPostTitle;
-                    var message = item.BlogPostMessage;
-                    var dateAdded = item.DateAdded;
-                    var dateEdited = item.DateEdited;
+                    switch (urlString.charAt(1)) {
+                        case 'A':
+                            RenderAdmin(item);
+                            break;
+                        case 'S':
+                            RenderUser(item);
+                            break;
+                        default:
+                            alert('How did you break it THAT badly?');
+                            return;
+                    }
                 });
+            $('#nonSearch').hide();
+            $('#posts2').show();
         },
         error: function (message) {
             alert(message.responseText);
@@ -38,4 +53,35 @@ function HandleSearch(urlString) {
         }
 
     });
+}
+
+function RenderAdmin(item) {
+    var row = '<div class="row">';
+    row += '<div class="col-md-1">';
+    row += '<p>' + item.BlogPostId + '</p>';
+    row += '</div>';
+    row += '<div class="col-md-4">';
+    row += '<h3>' + item.Title + '</h3>';
+    row += '</div>';
+    row += '<div class="col-md-2">';
+    row += '<p><a href="/admin/update/' + item.BlogPostId + '" class="btn-sm btn-primary">Update</a></p>';
+    row += '</div>';
+    row += '<div class="col-md-2">';
+    row += '<p><a href="/admin/delete/' + item.BlogPostId + '" class="btn-sm btn-danger">Delete</a></p>';
+    row += '</div></div>';
+    $('#posts2').append(row);
+}
+
+function RenderUser(item) {
+    var row =
+        '<div class="container"><div class="posts1"><div class="container"><div class="row"><div class="col-12"><h3>';
+    row += item.Title;
+    row += '</h3></div></div><div class="row"><div class="col-12">';
+    row += item.Message;
+    row += '</div></div><p></p></div><div class="col-md-12"><div class="row ending"><div class="col-md-4"><p>';
+    row += Date(item.DateAdded.match(/\d+/)).toLocaleString('en-US').match(/^[^G]+/); //What am I even doing at this point
+    row += '</p></div><div class="col-md-4"><p>';
+    row += Date(item.DateAdded.match(/\d+/)).toLocaleString('en-US').match(/^[^G]+/);
+    row += '</p></div><div class="col-md-4"><p>@theOwner</p></div></div></div></div><br /></div>';
+    $('.posts2').append(row);
 }
